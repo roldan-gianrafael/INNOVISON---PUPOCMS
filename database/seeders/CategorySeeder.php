1526ba5aa22d73ec2932fb223513f1da63dc1d3f
jusrivadenera16 <recohermosojustiner.pupm@gmail.com>
2026-03-29 14:52:08 +0800

    updated categories

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -11,14 +11,22 @@ class CategorySeeder extends Seeder
     {
         $categories = [
             ['code' => 'A', 'name' => 'Respiratory Disorder'],
-            ['code' => 'B', 'name' => 'GI Disorder'],
+            ['code' => 'B', 'name' => 'GI-L Disorders'],
             ['code' => 'C', 'name' => 'Musculo-Skeletal Disorder'],
             ['code' => 'D', 'name' => 'BP Monitoring'],
-            ['code' => 'E', 'name' => 'Cardio Disorder'],
+            ['code' => 'E', 'name' => 'Cardiovascular Disorder'],
+            ['code' => 'F', 'name' => 'CNS Disorder'],
+            ['code' => 'G', 'name' => 'Immune System Disorder'],
+            ['code' => 'H', 'name' => 'Derma Disorder'],
+            ['code' => 'I', 'name' => 'Surgery / Trauma'],
+            ['code' => 'J', 'name' => 'E.E.E.N.T Disorders'],
+            ['code' => 'K', 'name' => 'Reproductive Disorders'],
+            ['code' => 'L', 'name' => 'Nutritional Deficiency'],
+            ['code' => 'M', 'name' => 'Endocrine Disorders'],
+            ['code' => 'N', 'name' => 'Urinary Disorders'],
         ];
 
         foreach ($categories as $cat) {
-            // Ito ay mag-che-check kung may record na, kung wala pa, gagawa siya.
             Category::updateOrCreate(['code' => $cat['code']], $cat);
         }
     }

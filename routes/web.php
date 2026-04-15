639ce98f715c5267bf12f7327f42dfefe03f7580
jusrivadenera16 <recohermosojustiner.pupm@gmail.com>
2026-04-15 14:12:11 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::get('/auth/callback', [LoginController::class, 'handleIdpCallback'])->name('auth.callback');
 Route::post('/login-action', [LoginController::class, 'login']);
 Route::post('/register-action', [RegisterController::class, 'register']);
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout'); // Move here
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout'); 
 
 // --- PROTECTED ROUTES (Login required) ---
 Route::middleware([\Illuminate\Auth\Middleware\Authenticate::class, 'audit'])->group(function () {

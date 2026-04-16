7ff7b8286e1c1648bbead52928f27683289ba420
jusrivadenera16 <recohermosojustiner.pupm@gmail.com>
2026-04-16 08:51:45 +0800

    Update show_health.blade.php

--- resources/views/admin/show_health.blade.php
+++ resources/views/admin/show_health.blade.php
@@ -219,7 +219,7 @@
     <div class="section-header">PART I. STUDENT INFORMATION</div>
     <div class="row">
         <span class="label">Name:</span> <div class="field">{{ $profile->user->name }}</div>
-        <span class="label">Student No.:</span> <div class="field">{{ $profile->user->student_id }}</div>
+        <span class="label">Student No.:</span> <div class="field">{{ $profile->user->student_number }}</div>
     </div>
     <div class="row">
         <span class="label">Home Address:</span> <div class="field">{{ $profile->user->home_address ?? '' }}</div>

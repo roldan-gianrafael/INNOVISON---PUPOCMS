b041efee40c41d5d104d51cfbffba9f3dd12c142
jusrivadenera16 <recohermosojustiner.pupm@gmail.com>
2026-04-28 12:53:43 +0800

    Update appointments.blade.php

--- resources/views/admin/appointments.blade.php
+++ resources/views/admin/appointments.blade.php
@@ -330,7 +330,7 @@
         animation: fadeIn 0.2s;
     }
     .modal-box {
-        background: rgba(255, 255, 255, 0.7);
+        background: rgb(255, 255, 255);
         backdrop-filter: blur(20px);
         -webkit-backdrop-filter: blur(20px);
         padding: 24px;

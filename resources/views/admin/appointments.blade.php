b050a5c5ce7556086581dfde2f3bfefadc1a049a
jusrivadenera16 <recohermosojustiner.pupm@gmail.com>
2026-04-28 13:12:52 +0800

    Update appointments.blade.php

--- resources/views/admin/appointments.blade.php
+++ resources/views/admin/appointments.blade.php
@@ -351,7 +351,7 @@
         border-left: 1px solid rgba(112, 19, 27, 0.12) !important;
         border-right: 1px solid rgba(112, 19, 27, 0.12) !important;
         border-top: 4px solid #66ff00 !important;
-        border-bottom: 4px solid #66ff00 !important;
+        border-bottom: 4px solid #70131B !important;
         border-radius: 18px !important;
         backdrop-filter: blur(8px) !important;
         -webkit-backdrop-filter: blur(8px) !important;

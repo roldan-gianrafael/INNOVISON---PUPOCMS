ac4e30eb029ae5c86afb0471670daa2c37540a04
jusrivadenera16 <recohermosojustiner.pupm@gmail.com>
2026-04-28 13:30:21 +0800

    Update appointments.blade.php

--- resources/views/admin/appointments.blade.php
+++ resources/views/admin/appointments.blade.php
@@ -455,7 +455,7 @@
         font-weight: 700;
         cursor: pointer;
         transition: 0.2s;
-        border-bottom: 2px solid yellow;
+        border-bottom: 1px solid yellow;
         border-bottom-radius: 6px;
     }
     .dialog-btn-neutral {

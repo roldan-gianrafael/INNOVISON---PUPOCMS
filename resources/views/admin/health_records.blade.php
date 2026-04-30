07c3fee9e50abf439775107b2b208c2b74e6e1aa
jusrivadenera16 <recohermosojustiner.pupm@gmail.com>
2026-04-30 16:46:22 +0800

    Update health_records.blade.php

--- resources/views/admin/health_records.blade.php
+++ resources/views/admin/health_records.blade.php
@@ -186,7 +186,7 @@
         display: flex;
         align-items: center;
         width: 100%;
-        height: 40px;
+        height: 100px;
         box-sizing: border-box;
         padding: 15px 24px !important;
         border-radius: 12px;

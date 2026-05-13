ea666baa687b22b9e6033745acf851f2d3575e3f
jusrivadenera16 <recohermosojustiner.pupm@gmail.com>
2026-05-13 21:36:34 +0800

    Update student-assistant-portal.blade.php

--- resources/views/auth/student-assistant-portal.blade.php
+++ resources/views/auth/student-assistant-portal.blade.php
@@ -339,7 +339,7 @@
                         <span class="chooser-chip">Portal Choice</span>
                         <div class="chooser-icon" aria-hidden="true">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 1 0-11.18 0A17.93 17.93 0 0 0 12 21a17.93 17.93 0 0 0 7.59-6.63M15.59 14.37a6 6 0 1 1 0 0Z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a8.97 8.97 0 0 0 3.74-.8 4.5 4.5 0 0 0-7.48-2.33m3.74 3.13a9.06 9.06 0 0 1-6 0m6 0a8.96 8.96 0 0 1-6 0m0 0a8.97 8.97 0 0 1-3.74-.8 4.5 4.5 0 0 1 7.48-2.33M12 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
                             </svg>
                         </div>
                         <div>
@@ -362,7 +362,7 @@
                         <span class="chooser-chip">Portal Choice</span>
                         <div class="chooser-icon" aria-hidden="true">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 21h16.5M4.5 3h15a.75.75 0 0 1 .75.75v11.25a.75.75 0 0 1-.75.75h-15a.75.75 0 0 1-.75-.75V3.75A.75.75 0 0 1 4.5 3ZM9 21v-5.25m6 5.25v-5.25" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.983 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.072M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.552-.645-6.46-1.766l-.084-.049a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                             </svg>
                         </div>
                         <div>
